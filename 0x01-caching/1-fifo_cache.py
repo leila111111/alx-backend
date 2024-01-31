6b32dfcd4e06fa449906a192a185b8aa914b227a
leila111111 <leila_merzak@outlook.fr>
2024-01-31 23:27:56 +0000

    tasks

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,1 +1,30 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" FIFO caching
+"""
+from collections import OrderedDict
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """class FIFOCache that inherits
+    from BaseCaching and is a caching system
+    """
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Add item in the cache."""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD:", first_key)
+
+    def get(self, key):
+        """retrieves  item by key."""
+        return self.cache_data.get(key, None)
