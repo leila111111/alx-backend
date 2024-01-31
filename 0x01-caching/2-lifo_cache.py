f7922633a98c428ca25a46a2c590569b9cd6a342
leila111111 <leila_merzak@outlook.fr>
2024-01-31 23:31:25 +0000

    tasks

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,1 +1,32 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" LIFO Caching
+"""
+from collections import OrderedDict
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """class LIFOCache that inherits
+    from BaseCaching and is a caching system:
+    """
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Add item in the cache."""
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last_key)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
+
+    def get(self, key):
+        """retrieves  item by key."""
+        return self.cache_data.get(key, None)
